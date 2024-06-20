be594f106d4b5cf38bead362a47e2a3f46d1e945
luanastx <102376587+luanastx@users.noreply.github.com>
2024-06-19 23:40:35 -0300

    Update Paciente.cs

--- DesafioCSharpConsultorio/Models/Paciente.cs
+++ DesafioCSharpConsultorio/Models/Paciente.cs
@@ -7,10 +7,7 @@ namespace ConsultorioOdontologico.Models
         public string CPF { get; set; }
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
-<<<<<<< HEAD
         public int Idade => DateTime.Now.Year - DataNascimento.Year - (DateTime.Now.DayOfYear < DataNascimento.DayOfYear ? 1 : 0);
-=======
         public int Idade => DateTime.Now.Year - DataNascimento.Year;
->>>>>>> a0dc5e61dd2bf873b782059fa2d02b92ab301b79
     }
 }
